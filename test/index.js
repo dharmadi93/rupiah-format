c643b32b726495b1952fc66f73dd231f60c305e0
Dharmadi Tanamas <dharmadi93@gmail.com>
2016-11-16 10:45:26 +0700

    convert rupiah works

---
+++ test/index.js
@@ -0,0 +1,6 @@
+const convert = require('../index')
+
+console.log(convert.convert(123))
+console.log(convert.convert(12345))
+console.log(convert.convert(123456))
+console.log(convert.convert(1234567))
